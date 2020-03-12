804cc5c7a2acc738d3c17d18268092eb2acd269f
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-03-12 15:43:21 -0400

    config edit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    REACT_APP_API_ENDPOINT: 'https://serene-shelf-58339.herokuapp.com/api'
+    REACT_APP_API_ENDPOINT: 'https://serene-shelf-58339.herokuapp.com/'
     //REACT_APP_API_ENDPOINT: 'http://localhost:8080/'
 }
